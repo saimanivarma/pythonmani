672dabf63f3fe3a5d0127a0ebcb63928995adf1f
saimanivarma <varma338@gmail.com>
2021-07-04 18:48:51 +0530

    added sys module

--- func_sums_even.py
+++ func_sums_even.py
@@ -1,7 +1,7 @@
 # Write a function “sum_evens(to_num)” to calculate the sum of the even numbers
 # from 0 to the value of “to_num” passed to the function. Return the sum from the function.
 # Call the function inside print to see the result
-
+import sys
 def sums_evens(to_num):
     i = 0
     sum =0
@@ -10,7 +10,7 @@ def sums_evens(to_num):
             sum += i
         i+=1
     return sum
-to_num = int(input('enter a number: '))
+to_num = sys.argv[1]
 print(sums_evens(to_num))
 
 
